5a3c78611787400b7c63dee7494868b63a521637
Lucas Soria <104267584+lucasssoria@users.noreply.github.com>
2022-05-20 17:18:03 -0300

    consoleLogs ingrese 2 console logs para un mejor seguimiento de las posiciones en los Arrays

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 import "./styles.css";
+
 // 1er array para pedir los 6 primeros numeros
 let ingresoDeNumeros1: number[] = new Array(6);
+// Entra en bucle hasta que el usuario ingrese los 6 valores (en este caso ya determinado que son 6 y de tipo numericos)
 for (let i = 0; i < 6; i++) {
   let num1 = Number(
     prompt("Ingreso numero: " + (i + 1) + "\nIngrese sus 6 primeros numeros: ")
   );
   ingresoDeNumeros1[i] = num1;
+}
+// Muestro en consola para el seguimiento de los ingresos/valores
+for (let i = 0; i < ingresoDeNumeros1.length; i++) {
+  console.log(
+    "Posicion Array1: " + i + " Numero ingresado: " + ingresoDeNumeros1[i]
+  );
 }
 // 2do array para los 6 numeros a sumar
 let ingresoDeNumeros2: number[] = new Array(6);
@@ -14,6 +22,11 @@ for (let i = 0; i < 6; i++) {
     prompt("Ingreso numero: " + (i + 1) + "\nIngrese sus 6 numeros a sumar: ")
   );
   ingresoDeNumeros2[i] = num2;
+}
+for (let i = 0; i < ingresoDeNumeros2.length; i++) {
+  console.log(
+    "Posicion Array2: " + i + " Numero a sumar: " + ingresoDeNumeros2[i]
+  );
 }
 // 3er array para el resultado
 let sumaDeArrays = [];
@@ -24,15 +37,15 @@ for (let i = 0; i < 6; i++) {
 }
 
 // Otra manera de mostrar el resultado que encontre
-// Es resultado como array = al recorrido y suma de los arreglos 1 y 2
+// Resultado como array va a mutar/cambiar en el resultado de la suma de los arrays 1 y 2
 // let resultado = [];
 // for (let i = 0; i < 6; i++) {
 //   resultado.push(ingresoDeNumeros1[i] + ingresoDeNumeros2[i]);
 // }
 
-alert("El resultado de la suma de sus numero entre si, es: " + sumaDeArrays);
+alert("El resultado de la suma de sus numero entre si, es: \n" + sumaDeArrays);
 console.log(
-  "El resultado de la suma de sus numero entre si, es: " + sumaDeArrays
+  "El resultado de la suma de sus numero entre si, es: \n" + sumaDeArrays
 );
 
 // Recorrido para saber la longitud minima de los arreglos
