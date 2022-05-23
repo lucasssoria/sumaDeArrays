8773efc4947f8317cd44577be68863ff9b0d8ec7
Lucas Soria <104267584+lucasssoria@users.noreply.github.com>
2022-05-23 13:38:51 -0300

    updated se agrego la suma de los arrays con una funcion

---
+++ src/sumaDeArraysCFunciones.ts
@@ -0,0 +1,31 @@
+let array1: number[] = new Array(6);
+let array2: number[] = new Array(6);
+let nums1 = 0;
+let nums2 = 0;
+let sumaDeArrays = [0];
+function sumarArrays(nums1: number, nums2: number) {
+  for (let i = 0; i < 6; i++) {
+    nums1 = Number(
+      prompt("Ingreso numero: " + (i + 1) + "\nIngrese 6 numeros: ")
+    );
+    array1[i] = nums1;
+  }
+  console.log(array1);
+  for (let i = 0; i < 6; i++) {
+    nums2 = Number(
+      prompt("Ingreso numero: " + (i + 1) + "\nIngrese 6 numeros a sumar: ")
+    );
+    array2[i] = nums2;
+  }
+  console.log(array2);
+  let suma = 0;
+  for (let i = 0; i < 6; i++) {
+    suma = array1[i] + array2[i];
+    sumaDeArrays[i] = suma;
+  }
+  return sumaDeArrays;
+}
+sumarArrays(nums1, nums2);
+console.log(
+  "El resultado de la suma de sus numero entre si, es: \n" + sumaDeArrays
+);
