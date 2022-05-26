46202d0a7b34c14c056694c1a9b7dc3044ab22b5
Lucas Soria <104267584+lucasssoria@users.noreply.github.com>
2022-05-26 14:01:32 -0300

    arreglos

--- src/sumaDeArraysCFunciones.ts
+++ src/sumaDeArraysCFunciones.ts
@@ -1,30 +1,25 @@
 let array1: number[] = new Array(6);
 let array2: number[] = new Array(6);
 let sumaDeArrays: number[] = new Array(0);
-let nums1 = 0;
-let nums2 = 0;
-function sumarArrays(nums1: number, nums2: number) {
-  for (let i = 0; i < 6; i++) {
-    nums1 = Number(
-      prompt("Ingreso numero: " + (i + 1) + "\nIngrese 6 numeros: ")
-    );
-    array1[i] = nums1;
-  }
-  console.log(array1);
-  for (let i = 0; i < 6; i++) {
-    nums2 = Number(
-      prompt("Ingreso numero: " + (i + 1) + "\nIngrese 6 numeros a sumar: ")
-    );
-    array2[i] = nums2;
-  }
-  console.log(array2);
-  let suma = 0;
-  for (let i = 0; i < 6; i++) {
-    suma = array1[i] + array2[i];
-    sumaDeArrays[i] = suma;
-  }
+for (let i = 0; i < 6; i++) {
+  array1[i] = Number(
+    prompt("Ingreso numero: " + (i + 1) + "\nIngrese 6 numeros: ")
+  );
 }
-sumarArrays(nums1, nums2);
+console.log(array1);
+for (let i = 0; i < 6; i++) {
+  array2[i] = Number(
+    prompt("Ingreso numero: " + (i + 1) + "\nIngrese 6 numeros a sumar: ")
+  );
+}
+console.log(array2);
+function sumarArrays(array1: number[], array2: number[]) {
+  for (let i = 0; i < 6; i++) {
+    sumaDeArrays[i] = array1[i] + array2[i];
+  }
+  return sumaDeArrays;
+}
+sumarArrays(array1, array2);
 console.log(
   "El resultado de la suma de sus numero entre si, es: \n" + sumaDeArrays
 );
